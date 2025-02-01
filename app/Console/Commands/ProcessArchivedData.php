d6fbe4331f680d1e4182687844ecaa1cba7aaf6b
arslanpugc@gmail.com <arslanpugc@gmail.com>
2025-02-01 12:21:23 +0500

    archived cron time change

--- app/Console/Commands/ProcessArchivedData.php
+++ app/Console/Commands/ProcessArchivedData.php
@@ -29,7 +29,7 @@ class ProcessArchivedData extends Command
      */
     public function handle()
     {
-        $minutes = 250; // Time frame in minutes
+        $minutes = 1000; // Time frame in minutes
         $perPage = 100; // Records per page
         $baseUrl = 'http://carstat.dev/api/archived-lots';
         $totalPages = 0;
