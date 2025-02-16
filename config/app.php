5b1db3dfc27cdca2873fb77e569e86d623de01e1
= <=>
2025-02-16 14:43:32 +0500

    refactor: Redis Cache Implemented On ProcessFetchDatApi and ProcessCachedData Crons

--- config/app.php
+++ config/app.php
@@ -15,6 +15,8 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    'car_api_key' => env('CAR_API_KEY', ''),
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
