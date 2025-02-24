d0a8f9342eea8960be3b92a4e79aebd70f637657
= <=>
2025-02-24 21:41:45 +0500

    feat: Nats Setup Continue

--- app/Jobs/ProcessApiJob.php
+++ app/Jobs/ProcessApiJob.php
@@ -8,9 +8,8 @@ use Illuminate\Support\Str;
 class ProcessApiJob extends NatsMessageJob
 {
     public $queue = 'process-api-queue';
-    // Optionally, set a custom subject for your message.
 
-    // You can use a property to store dynamic content.
+    // Property to store dynamic content.
     protected $data;
 
     /**
@@ -20,7 +19,6 @@ class ProcessApiJob extends NatsMessageJob
      */
     public function __construct($data = null)
     {
-        // Store any dynamic data passed to the job.
         $this->data = $data;
     }
 
@@ -28,13 +26,13 @@ class ProcessApiJob extends NatsMessageJob
      * The body method returns the data that will be serialized
      * and sent as the message.
      *
-     * @return mixed
+     * @return string
      */
-    public function body(): mixed
+    public function body(): string
     {
-        return [
+        return json_encode([
             'data'         => $this->data ?? Str::random(32),
             'group_random' => random_int(1, 3),
-        ];
+        ]);
     }
 }
