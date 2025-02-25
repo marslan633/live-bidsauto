b84ba92ded17ea28085c371a4849276d4bc4bdb4
HafizMUmerFarooq <hafizmumerfarooq10@gmail.com>
2025-02-25 11:15:04 +0500

    Reverted back to Redis approach

--- app/Console/Commands/ProcessCachedDataOldThree.php
+++ app/Console/Commands/ProcessCachedDataOldThree.php
@@ -15,7 +15,7 @@ use App\Models\{
     State, City, Location, SellingBranch, Year, BuyNow, Odometer
 };
 
-class ProcessCachedDataThree extends Command
+class ProcessCachedDataOldThree extends Command
 {
     /**
      * The name and signature of the console command.
