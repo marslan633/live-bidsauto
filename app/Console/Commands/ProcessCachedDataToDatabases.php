c07790bb1aedfb8dbeda24828a99c597e4af6662
= <=>
2025-03-02 14:52:33 +0500

    test: Testing Process Cahced Data To Database Command

--- app/Console/Commands/ProcessCachedDataToDatabases.php
+++ app/Console/Commands/ProcessCachedDataToDatabases.php
@@ -51,7 +51,7 @@ class ProcessCachedDataToDatabases extends Command
 
             // Lock the cache keys for update
             $cacheKeys = RemoteCacheKey::where('cache_key', 'like', 'vehicle_data%')
-            ->where('status', 'progress')
+            ->where('status', 'pending')
             ->orderBy('created_at', 'asc')
             ->lockForUpdate()
             // ->take(10)
