1a9f13e94cf260e985584d878f88fb7f02e121bc
= <=>
2025-03-04 23:34:32 +0500

    test: Testing Process CachedData Api

--- app/Console/Commands/ProcessApiData.php
+++ app/Console/Commands/ProcessApiData.php
@@ -70,7 +70,7 @@ class ProcessApiData extends Command
             'updated_at' => now(),
         ]);
 
-        $perPage = 5;
+        $perPage = 1000;
         $baseUrl = 'http://carstat.dev/api/cars';
 
         if (config('app.is_full_fetch') === true) {
