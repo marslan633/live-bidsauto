d956159fbee8786af969d79838822478cc02fb56
= <=>
2025-03-08 02:44:01 +0500

    fix: Testing jobs

--- app/Jobs/ProcessCachedDataJob.php
+++ app/Jobs/ProcessCachedDataJob.php
@@ -24,7 +24,8 @@ class ProcessCachedDataJob implements ShouldQueue
     {
         $this->queue = 'process_cache_data_queue';
         $this->is_kvm_two = config('app.is_kvm_two');
-        $this->cacheKey = DB::connection($this->is_kvm_two ? 'mysql_remote' : 'mysql')->table('cache_keys')->find($cacheKey);
+        $this->cacheKey = DB::connection($this->is_kvm_two ? 'mysql_remote' : 'mysql')->table('cache_keys')->where('id', $cacheKey)
+        ->first();
     }
 
     /**
