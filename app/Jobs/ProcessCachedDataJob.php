d91e2ad74a1c3a28d142e7f6d5485dd777b3d78e
= <=>
2025-03-08 00:48:53 +0500

    fix: Testing jobs

--- app/Jobs/ProcessCachedDataJob.php
+++ app/Jobs/ProcessCachedDataJob.php
@@ -16,6 +16,8 @@ class ProcessCachedDataJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    public $queue = 'process_cache_data_queue';
+
     public $cacheKey;
 
     public function __construct(CacheKey $cacheKey)
