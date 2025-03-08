5ec2eb30ef0939348ed1a95d08d7782a95aaa314
= <=>
2025-03-09 00:19:20 +0500

    optim: Redis Seprate Databases Created For Storing Cache and Running Queues Connections

--- app/Console/Commands/ProcessCachedData.php
+++ app/Console/Commands/ProcessCachedData.php
@@ -60,6 +60,7 @@ public function handle()
         // IF KVM_ONE THAN USE DEFAULT DATABASE CONNECTION
         $IS_KVM_TWO = config('app.is_kvm_two');
         $CacheModel = $IS_KVM_TWO ? DB::connection('mysql_remote')->table('cache_keys') : DB::connection('mysql')->table('cache_keys');
+        Log::info('IS_KVM_TWO ' . config('app.is_kvm_two'));
         Log::info('Cache Model ' . $IS_KVM_TWO ? 'REMOTE_CACHE_KEY' : 'CACHE_KEY');
         $this->info('Cache Model ' . $IS_KVM_TWO ? 'REMOTE_CACHE_KEY' : 'CACHE_KEY');
         $keyName = $IS_KVM_TWO ? 'vehicle_process_data_' : 'vehicle_api_data_';
