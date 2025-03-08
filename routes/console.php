d871044105e8559d2985faa912bc51bc8b1a8609
= <=>
2025-03-09 03:38:05 +0500

    optim: Redis Seprate Databases Created For Storing Cache and Running Queues Connections

--- routes/console.php
+++ routes/console.php
@@ -20,7 +20,7 @@ if(config('app.app_kvm_one') === true){
         Log::info('process:api-data completed successfully.');
 
         // Dispatch the process:cached-data command
-        app(Schedule::class)->command('process:cached-data');
+        Artisan::call('process:cached-data');
     })
     ->onFailure(function () {
         // Log the failure of process:api-data
