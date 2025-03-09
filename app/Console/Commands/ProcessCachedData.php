2068b548d6a3fff6ecb3a5a11ef25e7b949400fd
= <=>
2025-03-09 15:10:30 +0500

    optim: Redis Seprate Databases Created For Storing Cache and Running Queues Connections

--- app/Console/Commands/ProcessCachedData.php
+++ app/Console/Commands/ProcessCachedData.php
@@ -42,12 +42,19 @@ public function handle()
 {
     $startDateTime = Carbon::now();
     $this->info("Process started at: " . $startDateTime);
-    \Log::info("Process started at: " . $startDateTime);
+    Log::info("Process started at: " . $startDateTime);
+
+    $IS_KVM_TWO = config('app.is_kvm_two');
 
     try {
-        DB::connection('mysql')->beginTransaction();
-        DB::connection('mysql_remote')->beginTransaction();
-        $cronRun = DB::table('cron_run_history')->insertGetId([
+        if($IS_KVM_TWO === true){
+            DB::connection('mysql_remote')->beginTransaction();
+        }else{
+            DB::connection('mysql')->beginTransaction();
+        }
+
+        // Always Will Run On Default Server
+        $cronRun = DB::connection('mysql')->table('cron_run_history')->insertGetId([
             'cron_name' => 'process_cached_data',
             'start_time' => $startDateTime,
             'status' => 'running',
@@ -58,26 +65,23 @@ public function handle()
         // Lock the cache keys for update
         // IF KVM_TWO THAN USE REMOTE DATABSE CONNECTION
         // IF KVM_ONE THAN USE DEFAULT DATABASE CONNECTION
-        $IS_KVM_TWO = config('app.is_kvm_two');
         $CacheModel = $IS_KVM_TWO === true ? DB::connection('mysql_remote')->table('cache_keys') : DB::connection('mysql')->table('cache_keys');
-        Log::info('IS_KVM_TWO ' . config('app.is_kvm_two'));
-        Log::info('Cache Model ' . $IS_KVM_TWO === true ? 'REMOTE_CACHE_KEY' : 'CACHE_KEY');
-        $this->info('Cache Model ' . $IS_KVM_TWO === true ? 'REMOTE_CACHE_KEY' : 'CACHE_KEY');
-        $this->info('IS_KVM_TWO type => ' . gettype(config('app.is_kvm_two')) . ' ' . config('app.is_kvm_two') === true ? 'Yes' : 'No');
 
-        $keyName = $IS_KVM_TWO === true ? 'vehicle_process_data_' : 'vehicle_api_data_';
-        $cacheKeys = $CacheModel->where('cache_key', 'like', $keyName.'%')
+        $this->info('Cache Model ' . $IS_KVM_TWO === true ? 'REMOTE_CACHE_KEY' : 'CACHE_KEY');
+
+        $cacheKeys = $CacheModel->where('cache_key', 'like', 'vehicle_api_data_%')
             ->where('status', 'pending')
             ->orderBy('created_at', 'asc')
-            // ->lockForUpdate()
-            // ->skipLocked()
             ->take(30)
             ->get();
 
         if ($cacheKeys->isEmpty()) {
             $this->info("No pending cache keys found.");
-            DB::connection('mysql')->commit();
-            DB::connection('mysql_remote')->commit();
+            if($IS_KVM_TWO === true){
+                DB::connection('mysql_remote')->commit();
+            }else{
+                DB::connection('mysql')->commit();
+            }
             return;
         }
 
@@ -85,11 +89,17 @@ public function handle()
 
         // Update status in bulk
         $CacheModel->whereIn('id', $cacheKeyIds)->update(['status' => 'progress']);
-        DB::connection('mysql')->commit();
-        DB::connection('mysql_remote')->commit();
+        if($IS_KVM_TWO === true){
+            DB::connection('mysql_remote')->commit();
+        }else{
+            DB::connection('mysql')->commit();
+        }
     } catch (\Exception $e) {
-        DB::connection('mysql')->rollBack();
-        DB::connection('mysql_remote')->rollBack();
+        if($IS_KVM_TWO === true){
+            DB::connection('mysql_remote')->rollBack();
+        }else{
+            DB::connection('mysql')->rollBack();
+        }
         Log::info("Error fetching cache keys: ", ['data' => json_encode($e->getMessage())]);
         $this->handleCronError($cronRun, "Error fetching cache keys: " . $e->getMessage());
         return;
@@ -163,6 +173,7 @@ public function handle()
 private function handleCronError($cronRun, $errorMessage)
 {
     Log::error($errorMessage);
+    // Always Run on Defautl Server
     DB::connection('mysql')->table('cron_run_history')->where('id', $cronRun)->update([
         'end_time' => Carbon::now(),
         'status' => 'failed',
