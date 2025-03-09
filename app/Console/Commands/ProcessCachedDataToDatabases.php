78e08e85d0590c781f0fb8d2b4f7b0ca12264d64
= <=>
2025-03-09 15:13:40 +0500

    optim: Redis Seprate Databases Created For Storing Cache and Running Queues Connections

--- app/Console/Commands/ProcessCachedDataToDatabases.php
+++ app/Console/Commands/ProcessCachedDataToDatabases.php
@@ -63,7 +63,7 @@ class ProcessCachedDataToDatabases extends Command
             ->where('status', 'pending')
             ->orderBy('created_at', 'asc')
             // ->lockForUpdate()
-            ->take(30)
+            ->take(5)
             ->get();
 
             if ($cacheKeys->isEmpty()) {
