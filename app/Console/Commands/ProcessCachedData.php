41370d99dce282c7e6c8f6b71af7ba5a42b4a277
= <=>
2025-03-09 14:44:50 +0500

    optim: Redis Seprate Databases Created For Storing Cache and Running Queues Connections

--- app/Console/Commands/ProcessCachedData.php
+++ app/Console/Commands/ProcessCachedData.php
@@ -45,8 +45,8 @@ public function handle()
     \Log::info("Process started at: " . $startDateTime);
 
     try {
-        // DB::connection('mysql')->beginTransaction();
-        // DB::connection('mysql_remote')->beginTransaction();
+        DB::connection('mysql')->beginTransaction();
+        DB::connection('mysql_remote')->beginTransaction();
         $cronRun = DB::table('cron_run_history')->insertGetId([
             'cron_name' => 'process_cached_data',
             'start_time' => $startDateTime,
@@ -76,8 +76,8 @@ public function handle()
 
         if ($cacheKeys->isEmpty()) {
             $this->info("No pending cache keys found.");
-            // DB::connection('mysql')->commit();
-            // DB::connection('mysql_remote')->commit();
+            DB::connection('mysql')->commit();
+            DB::connection('mysql_remote')->commit();
             return;
         }
 
@@ -85,11 +85,11 @@ public function handle()
 
         // Update status in bulk
         $CacheModel->whereIn('id', $cacheKeyIds)->update(['status' => 'progress']);
-        // DB::connection('mysql')->commit();
-        // DB::connection('mysql_remote')->commit();
+        DB::connection('mysql')->commit();
+        DB::connection('mysql_remote')->commit();
     } catch (\Exception $e) {
-        // DB::connection('mysql')->rollBack();
-        // DB::connection('mysql_remote')->rollBack();
+        DB::connection('mysql')->rollBack();
+        DB::connection('mysql_remote')->rollBack();
         Log::info("Error fetching cache keys: ", ['data' => json_encode($e->getMessage())]);
         $this->handleCronError($cronRun, "Error fetching cache keys: " . $e->getMessage());
         return;
@@ -98,58 +98,56 @@ public function handle()
 
     foreach ($cacheKeys as $cacheKey) {
         $this->info('Cache Key Running: ' . $cacheKey->cache_key);
-            ProcessCachedDataJob::dispatch($cacheKey->id);
-        $this->info('After Passing to Job: ' . $cacheKey->cache_key);
-        // try{
-        //     $CacheModel = $IS_KVM_TWO === true ? DB::connection('mysql_remote')->table('cache_keys') : DB::connection('mysql')->table('cache_keys');
+        try{
+            $CacheModel = $IS_KVM_TWO === true ? DB::connection('mysql_remote')->table('cache_keys') : DB::connection('mysql')->table('cache_keys');
 
-        //     $key = $cacheKey->cache_key;
-        //     // IF KVM_TWO than Read it from Remote Redis
-        //     // IF KVM_ONE than Read it from Default Redis
-        //     $data = Cache::store($IS_KVM_TWO === true ? 'redis_cache' : 'redis')->get($key);
+            $key = $cacheKey->cache_key;
+            // IF KVM_TWO than Read it from Remote Redis
+            // IF KVM_ONE than Read it from Default Redis
+            $data = Cache::store($IS_KVM_TWO === true ? 'redis_cache' : 'redis')->get($key);
 
-        //     if (!$data) {
-        //         Log::info('Data Not Found');
-        //         $this->info('Data not found');
-        //         return;
-        //     }
+            if (!$data) {
+                Log::info('Data Not Found');
+                $this->info('Data not found');
+                return;
+            }
 
-        //     $processDataForCache = [];
-        //     foreach ($data as $car) {
-        //         $processedCar = convertAndStoreDataToRedis($car);
-        //         $processDataForCache[] = $processedCar;
-        //     }
+            $processDataForCache = [];
+            foreach ($data as $car) {
+                $processedCar = convertAndStoreDataToRedis($car);
+                $processDataForCache[] = $processedCar;
+            }
 
-        //     // Store processed data in Redis
-        //     $cacheKey = 'vehicle_process_data_' . now()->format('Y_m_d_H_i_s');
-        //     $expiresAt = now()->addMinutes(intval(config('app.cache_key_expiry')));
-        //     // IF KVM_TWO THAN Write IT FROM DEFAULT
-        //     // IF KVM_ONE THAN Write IT FROM REMIVE
-        //     Cache::store($IS_KVM_TWO === true ? 'redis' : 'redis_cache')->put($cacheKey, json_encode($processDataForCache), $expiresAt);
+            // Store processed data in Redis
+            $cacheKey = 'vehicle_process_data_' . now()->format('Y_m_d_H_i_s');
+            $expiresAt = now()->addMinutes(intval(config('app.cache_key_expiry')));
+            // IF KVM_TWO THAN Write IT FROM DEFAULT
+            // IF KVM_ONE THAN Write IT FROM REMIVE
+            Cache::store($IS_KVM_TWO === true ? 'redis' : 'redis_cache')->put($cacheKey, json_encode($processDataForCache), $expiresAt);
 
-        //     // Save cache details to the database
-        //     // IF KVM_TWO THAN USE DEFAULT DATABASE CONNECTION
-        //     // IF KVM_ONE THAN USE REMOTE DATABASE CONNECTION
-        //     $RemoteCacheModel = $IS_KVM_TWO === true  ? DB::connection('mysql')->table('cache_keys') : DB::connection('mysql_remote')->table('cache_keys');
-        //         Log::info('Remote Cache Model Job ' . $IS_KVM_TWO === true ? 'CACHE_KEY' : 'REMOTE_CACHE_KEY');
+            // Save cache details to the database
+            // IF KVM_TWO THAN USE DEFAULT DATABASE CONNECTION
+            // IF KVM_ONE THAN USE REMOTE DATABASE CONNECTION
+            $RemoteCacheModel = $IS_KVM_TWO === true  ? DB::connection('mysql')->table('cache_keys') : DB::connection('mysql_remote')->table('cache_keys');
+                Log::info('Remote Cache Model Job ' . $IS_KVM_TWO === true ? 'CACHE_KEY' : 'REMOTE_CACHE_KEY');
 
-        //     $RemoteCacheModel->updateOrInsert(
-        //         ['cache_key' => $cacheKey],
-        //         ['status' => 'pending', 'expires_at' => $expiresAt]
-        //     );
+            $RemoteCacheModel->updateOrInsert(
+                ['cache_key' => $cacheKey],
+                ['status' => 'pending', 'expires_at' => $expiresAt]
+            );
 
-        //     // Remove cache key from DB and Redis
-        //     $CacheModel->where('cache_key', $key)->delete();
-        //     // IF KVM_TWO THAN REMOVE IT FROM REMOTE
-        //     // IF KVM_ONE THAN REMOVE IT FROM DEFAULT
-        //     Cache::store($IS_KVM_TWO === true ? 'redis_cache' :'redis')->forget($key);
-        //     $this->info('Key Stored: '. $key);
+            // Remove cache key from DB and Redis
+            $CacheModel->where('cache_key', $key)->delete();
+            // IF KVM_TWO THAN REMOVE IT FROM REMOTE
+            // IF KVM_ONE THAN REMOVE IT FROM DEFAULT
+            Cache::store($IS_KVM_TWO === true ? 'redis_cache' :'redis')->forget($key);
+            $this->info('Key Stored: '. $key);
 
-        // }catch(\Exception $e){
-        //     DB::connection('mysql')->table('cache_keys')->where('id',$cacheKey->id)->update(['status' => 'pending']);
-        //     $this->info("Error processing key {$cacheKey->cache_key}: ");
-        //     Log::info("Error processing key {$cacheKey->cache_key}: " . $e->getMessage());
-        // }
+        }catch(\Exception $e){
+            DB::connection('mysql')->table('cache_keys')->where('id',$cacheKey->id)->update(['status' => 'pending']);
+            $this->info("Error processing key {$cacheKey->cache_key}: ");
+            Log::info("Error processing key {$cacheKey->cache_key}: " . $e->getMessage());
+        }
     }
 
     DB::connection('mysql')->table('cron_run_history')->where('id', $cronRun)->update([
