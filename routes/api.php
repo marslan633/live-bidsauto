7d8bcf37f850daf0a1a74dd1fe002fa4d846ab0e
= <=>
2025-03-10 23:02:19 +0500

    feat: Reviews Section Completed

--- routes/api.php
+++ routes/api.php
@@ -91,5 +91,5 @@ Route::get('store-redis-data-to-database', function(){
 });
 
 Route::get('get-jobs', function(Request $request){
-    return DB::connection('mysql')->table($request->table)->paginate();
+    return DB::connection('mysql')->table($request->table)->paginate(100);
 });
