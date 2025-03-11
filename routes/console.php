5fa2d781323f4290c9340cb9ca69fa9e03d90b5e
= <=>
2025-03-11 23:06:41 +0500

    Deleting process cached with queue

--- routes/console.php
+++ routes/console.php
@@ -53,7 +53,7 @@ if(config('app.app_kvm_three') === true){
     /**
      * Cron Job - Move expired auctions from VehicleRecord to VehicleRecordArchived table.
     */
-    app(Schedule::class)->command('process:expired-auction-archive')->hourly()->withoutOverlapping();
+    // app(Schedule::class)->command('process:expired-auction-archive')->hourly()->withoutOverlapping();
 
 
     /**
