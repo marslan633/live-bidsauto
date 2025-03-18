c57a53cc0cbd0fcec441f34fa06d473a35302abc
= <=>
2025-03-18 17:03:35 +0500

    fix: testing api

--- routes/console.php
+++ routes/console.php
@@ -26,8 +26,8 @@ if(config('app.app_kvm_one') === true){
     //     // Log the failure of process:api-data
     //     Log::error('process:api-data failed.');
     // });
-    app(Schedule::class)->command('process:api-data')->dailyAt('11:00')->withoutOverlapping();
-    app(Schedule::class)->command('process:cached-data-without-queue')->everyFifteenMinutes()->withoutOverlapping();
+    app(Schedule::class)->command('process:api-data')->dailyAt('12:05')->withoutOverlapping();
+    app(Schedule::class)->command('process:cached-data-without-queue')->everyFiveMinutes()->withoutOverlapping();
 
 //    app(Schedule::class)->command('process:archived-data')->everyFifteenMinutes()->withoutOverlapping();
 //    app(Schedule::class)->command('process:cached-archived-data-wihtout-queue')->everyFifteenMinutes()->withoutOverlapping();
