ceae113e6037c7507747fc48fff2b3805615bbd3
= <=>
2025-03-19 23:54:18 +0500

    feat: storing data with api

--- app/Console/Commands/ProcessCachedDataToDatabases.php
+++ app/Console/Commands/ProcessCachedDataToDatabases.php
@@ -38,22 +38,45 @@ class ProcessCachedDataToDatabases extends Command
         Log::info("Process started at: " . $startDateTime);
 
         $cronRun = null;
+        $cronRun = '';
+
+        $url = config('app.cron_history_api_url') . '/cron-run-histories';
+        $apiUrl = config('app.cron_history_api_url') . '/get-vehicles-for-database';
 
         try{
             // Remote Connection to KVM4.1
-
-            $cronRunRecord = CronRunHistory::create([
+            $cronRunResponse = Http::post($url, [
                 'cron_name' => 'process_cached_data_to_database',
                 'start_time' => now(),
                 'status' => 'running',
             ]);
-            $cronRun = $cronRunRecord->_id ?? null;
 
-            if (!VehicleProcessCachedApiData::exists()) {
+            if ($cronRunResponse->successful()) {
+                // Handle the successful API cronRunResponse
+                $cronRun = $cronRunResponse->json()['id'] ?? null; // You can process the data as needed
+                // Optionally, you can update the cron record with the API response or status
+            } else {
+                Log::info('Create Cron Run History Not Working');
+            }
+
+
+            // **Fetch Fresh Data from API**
+            $response = Http::timeout(120)
+                ->retry(3, 1000)
+                ->get($apiUrl);
+
+            if (!$response->successful()) {
+                $this->info("Error In Fetch Data Api Call");
+                $this->info("Error In Fetch Data Api Call");
+                return;
+            }
+            $data = $response->json()['data'] ?? [];
+
+            if (count($data)) {
+                $this->info("No Data Pending to process");
                 $this->info("No Data Pending to process");
                 return;
             }
-
 
         }catch(\Exception $e){
             // if($cronRun !== null){
@@ -64,21 +87,29 @@ class ProcessCachedDataToDatabases extends Command
 
         // **Batch processing setup**
         // Initialize an empty array to hold the jobs
-        VehicleProcessCachedApiData::orderBy('_id')
-            ->take(100) // Fetch only the first 100 rows
-            ->chunkById(10, function ($cacheKeys) {
-                foreach ($cacheKeys as $itemKey) {
-                    ProcessCachedDataToDatabaseJob::dispatch($itemKey);
-                }
-            });
+        collect($data)->chunk(10)->each(function ($chunk) {
+            foreach ($chunk as $item) {
+                // Dispatch a job for each item in the chunk
+                ProcessCachedDataToDatabaseJob::dispatch($item);
+            }
+        });
 
         if($cronRun){
 
-            CronRunHistory::where('_id', new ObjectId($cronRun))->update([
+            $updateUrl = $url . "/$cronRun";
+             // Remote Connection to KVM4.1
+             $cronRunUpdateResponse = Http::post($updateUrl, [
                 'end_time' => Carbon::now(),
                 'status' => 'success',
                 'updated_at' => now(),
             ]);
+
+            if ($cronRunUpdateResponse->successful()) {
+                Log::info('Cron Run History Updating Successfully!');
+            } else {
+                Log::info('Update Cron Run History Not Working');
+            }
+
         }
 
 
@@ -90,13 +121,21 @@ class ProcessCachedDataToDatabases extends Command
     private function handleCronError($cronRun, $errorMessage)
     {
         Log::error($errorMessage);
-        $apiUrl = config('app.cron_history_api_url') . '/api/cron-run-histories';
+        $url = config('app.cron_history_api_url') . '/cron-run-histories';
+        $updateUrl = $url . "/$cronRun";
+             // Remote Connection to KVM4.1
+             $cronRunUpdateResponse = Http::post($updateUrl, [
+                'end_time' => Carbon::now(),
+                'status' => 'failed',
+                'updated_at' => now(),
+            ]);
 
-        CronRunHistory::where('_id', new ObjectId($cronRun))->update([
-            'end_time' => Carbon::now(),
-            'status' => 'failed',
-            'updated_at' => now(),
-        ]);
+            if ($cronRunUpdateResponse->successful()) {
+                Log::info('Cron Run History Updating Successfully!');
+            } else {
+                Log::info('Update Cron Run History Not Working');
+            }
+
 
         $adminEmails = explode(',', env('ADMIN_EMAIL'));
         Mail::to($adminEmails)->send(new CronJobFailedMail($errorMessage, 'process_cached_data'));
