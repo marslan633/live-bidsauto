1b7803f43291afe3edbb2787aa1c16f99873a5ed
= <=>
2025-03-21 00:22:35 +0500

    optimz: testing code

--- app/Jobs/ProcessCachedArchivedDataJob.php
+++ app/Jobs/ProcessCachedArchivedDataJob.php
@@ -144,19 +144,19 @@ class ProcessCachedArchivedDataJob implements ShouldQueue
 
 
             // DB::commit();
-            $url = config('app.cron_history_api_url') . "/delete/vehicle-archived-record/" . $this->cacheKey->id;
+            $url = config('app.cron_history_api_url') . "/delete-my-archive-vehicle/$this->cacheKey->id";
             Log::info('DELET API URL: ', ['url' => $url]);
-            // $cronRunUpdateResponse = Http::timeout(120)->retry(3, 1000)->post($url, [
-            //     'end_time' => Carbon::now(),
-            //     'status' => 'success',
-            //     'updated_at' => now(),
-            // ]);
+            $cronRunUpdateResponse = Http::timeout(120)->retry(3, 1000)->post($url, [
+                'end_time' => Carbon::now(),
+                'status' => 'success',
+                'updated_at' => now(),
+            ]);
 
-            // if ($cronRunUpdateResponse->successful()) {
-            //     Log::info('Vehicle Process Cached Api Data Delete');
-            // } else {
-            //     Log::info('ERROR: Vehicle Process Cached Api Data Delete');
-            // }
+            if ($cronRunUpdateResponse->successful()) {
+                Log::info('Vehicle Process Cached Api Data Delete');
+            } else {
+                Log::info('ERROR: Vehicle Process Cached Api Data Delete');
+            }
             Log::info("Batch processed successfully with " . count($updatedRecords) . " updated records.");
         } catch (\Exception $e) {
             // DB::rollBack();
