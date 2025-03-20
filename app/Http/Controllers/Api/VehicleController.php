a83789988fa8664bbd891e0f9ba52ae75fa955e7
= <=>
2025-03-20 23:16:16 +0500

    optimz: testing code

--- app/Http/Controllers/Api/VehicleController.php
+++ app/Http/Controllers/Api/VehicleController.php
@@ -39,7 +39,7 @@ class VehicleController extends Controller
 
     public function getVechiclesForDatabase(){
         try{
-            $data = VehicleProcessCachedApiData::orderBy('created_at', 'desc')->paginate(1);
+            $data = VehicleProcessCachedApiData::orderBy('created_at', 'desc')->paginate(intval(config('app.per_page_vehicle_data')));
             return sendResponse(true, 200, 'Vehicles Detail Fetched Successfully!', $data, 200);
 
         } catch (\Exception $ex) {
@@ -49,7 +49,7 @@ class VehicleController extends Controller
 
     public function getArchivedVechiclesForDatabase(){
         try{
-            $data = VehicleArchivedApiData::orderBy('created_at', 'asc')->paginate(1);
+            $data = VehicleArchivedApiData::orderBy('created_at', 'asc')->paginate(intval(config('app.per_page_archived_data')));
             return sendResponse(true, 200, 'Archived Vehicles Detail Fetched Successfully!', $data, 200);
 
         } catch (\Exception $ex) {
--- config/app.php
+++ config/app.php
@@ -24,6 +24,8 @@ return [
     'batch_size' => env('BATCH_SIZE', 1000),
     'cache_key_expiry' => env('CACHE_KEY_EXPIRY', 1000),
     'ttl_expiry' => env('TTL_EXPIRY', 7),
+    'per_page_vehicle_data' => env('PER_PAGE_VEHICLE_DATA', 10),
+    'per_page_archived_data' => env('PER_PAGE_ARCHIVED_DATA', 10),
     'cron_history_api_url' => env('CRON_HISTORY_API_URL'),
 
     /*
