80a218090d81ef874b5435c673bf52be89d3360d
= <=>
2025-03-21 00:30:47 +0500

    optimz: testing code

--- routes/console.php
+++ routes/console.php
@@ -26,10 +26,10 @@ if (config('app.app_kvm_one') === true) {
     //     // Log the failure of process:api-data
     //     Log::error('process:api-data failed.');
     // });
-    app(Schedule::class)->command('process:api-data')->dailyAt('00:10')->withoutOverlapping();
+    app(Schedule::class)->command('process:api-data')->dailyAt('19:40')->withoutOverlapping();
     app(Schedule::class)->command('process:cached-data-without-queue')->everyFiveMinutes()->withoutOverlapping();
 
-    //    app(Schedule::class)->command('process:archived-data')->everyFifteenMinutes()->withoutOverlapping();
+    // app(Schedule::class)->command('process:archived-data')->everyFifteenMinutes()->withoutOverlapping();
 
 }
 
@@ -38,7 +38,7 @@ if (config('app.app_kvm_three') === true) {
     /**
      * Cron Job - Process Vehicle Data from kvm4.2 redis cache and populate it into kvm4.3 Mysql.
      */
-    // app(Schedule::class)->command('process:process-cached-data-to-databases')->everyFifteenMinutes()->withoutOverlapping();
+    app(Schedule::class)->command('process:process-cached-data-to-databases')->everyTenMinutes()->withoutOverlapping();
 
     /**
     * Cron Job - Update the data (bid, final_bid_updated_at, status) of archived vehicle table on the base of third party api.
