8efd6fa494160ac208ea6ccaf93833ecd9c2c922
= <=>
2025-04-17 12:13:53 +0500

    feat: Cron Run Logic Added To Index Vehicles Api

--- routes/console.php
+++ routes/console.php
@@ -27,8 +27,8 @@ if (config('app.app_kvm_one') === true) {
     //     // Log the failure of process:api-data
     //     Log::error('process:api-data failed.');
     // });
-    app(Schedule::class)->command('process:api-data')->dailyAt('11:20')->withoutOverlapping();
-    app(Schedule::class)->command('process:cached-data-without-queue')->everyFiveMinutes()->withoutOverlapping();
+    // app(Schedule::class)->command('process:api-data')->dailyAt('11:20')->withoutOverlapping();
+    // app(Schedule::class)->command('process:cached-data-without-queue')->everyFiveMinutes()->withoutOverlapping();
 
     // app(Schedule::class)->command('process:archived-data')->everyFifteenMinutes()->withoutOverlapping();
 
@@ -39,7 +39,7 @@ if (config('app.app_kvm_three') === true) {
     /**
      * Cron Job - Process Vehicle Data from kvm4.2 redis cache and populate it into kvm4.3 Mysql.
      */
-    app(Schedule::class)->command('process:process-cached-data-to-databases-without-queue')->everyTenMinutes()->withoutOverlapping();
+    // app(Schedule::class)->command('process:process-cached-data-to-databases-without-queue')->everyTenMinutes()->withoutOverlapping();
 
     //app(Schedule::class)->command('auction:restore-archived')->dailyAt('09:43')->withoutOverlapping();
 
