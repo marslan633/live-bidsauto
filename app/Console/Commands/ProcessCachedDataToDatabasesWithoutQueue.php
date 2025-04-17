471bb3a2e33fb61d9cf8aa512dffbe9bbe9c383f
= <=>
2025-04-17 17:11:19 +0500

    feat: Cron Run Logic Added To Index Vehicles Api

--- app/Console/Commands/ProcessCachedDataToDatabasesWithoutQueue.php
+++ app/Console/Commands/ProcessCachedDataToDatabasesWithoutQueue.php
@@ -550,16 +550,26 @@ class ProcessCachedDataToDatabasesWithoutQueue extends Command
             }
 
             // ✅ Bulk Insert New Records
-            if (!empty($newRecords)) {
-                DB::table('vehicle_records')->insert($newRecords);
-            }
+            // if (!empty($newRecords)) {
+            //     DB::table('vehicle_records')->insert($newRecords);
+            // }
 
-            // ✅ Bulk Update Existing Records
-            if (!empty($updatedRecords)) {
-                foreach($updatedRecords as $item){
-                    DB::table('vehicle_records')->where('id', $item['id'])->update($item);
-                }
-                // DB::table('vehicle_records')->upsert($updatedRecords, ['id'], array_keys($updatedRecords[0]));
+            // // ✅ Bulk Update Existing Records
+            // if (!empty($updatedRecords)) {
+            //     foreach($updatedRecords as $item){
+            //         DB::table('vehicle_records')->where('id', $item['id'])->update($item);
+            //     }
+            //     // DB::table('vehicle_records')->upsert($updatedRecords, ['id'], array_keys($updatedRecords[0]));
+            // }
+
+
+            if (!empty($newRecords) || !empty($updatedRecords)) {
+                $mergedRecords = array_merge($newRecords, $updatedRecords);
+                DB::table('vehicle_records')->upsert(
+                    $mergedRecords,
+                    ['api_id'], // Unique key to determine insert or update
+                    array_keys($mergedRecords[0]) // Columns to update if exists
+                );
             }
 
             $url = config('app.cron_history_api_url') . "/delete-my-vehicle/$cacheKey";
