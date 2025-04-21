66ac5d8d9edb6eaf907277635683e7fd61283636
= <=>
2025-04-21 14:16:32 +0500

    test: Testing Get Vehicles Api

--- app/Http/Controllers/Api/VehicleController.php
+++ app/Http/Controllers/Api/VehicleController.php
@@ -291,7 +291,7 @@ class VehicleController extends Controller
 
             // Buy Now Filter
             if ($request->has('buy_now')) {
-                $buyNow = (int) $request->input('buy_now');
+                $buyNow = filter_var($request->input('buy_now'), FILTER_VALIDATE_BOOLEAN);
                 if ($buyNow >= 1) {
                     $buyNowId = BuyNow::where('name', 'buyNowWithPrice')->value('id');
                     $must[] = ['term' => ['buy_now_id' => (int) $buyNowId]];
