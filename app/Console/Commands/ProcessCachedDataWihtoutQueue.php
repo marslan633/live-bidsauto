75b130a796ec8a422138987052e8a5ee9ce2a251
= <=>
2025-04-23 11:05:36 +0500

    fix: fixing filter attributes api

--- app/Console/Commands/ProcessCachedDataWihtoutQueue.php
+++ app/Console/Commands/ProcessCachedDataWihtoutQueue.php
@@ -47,7 +47,7 @@ class ProcessCachedDataWihtoutQueue extends Command
                 'updated_at' => now(),
             ])->_id;
 
-            $cacheKeys = VehicleApiData::orderBy('created_at', 'desc')->limit(500)->get();
+            $cacheKeys = VehicleApiData::orderBy('created_at', 'desc')->limit(1000)->get();
             if (count($cacheKeys) == 0) {
                 $this->info("No pending cache keys found.");
                 return;
