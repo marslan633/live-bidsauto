a8bc1af656947adc4b52f17629f57ada22c47998
= <=>
2025-04-24 18:02:35 +0500

    feat: Index Vehicles Records Converted to job

---
+++ app/Jobs/IndexVehicleRecordArchivedsJob.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use App\Models\VehicleRecordArchived;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class IndexVehicleRecordArchivedsJob implements ShouldQueue
+{
+    use Queueable;
+
+    protected $cronRun = null;
+    protected $minutes;
+
+    public function __construct()
+    {
+        $this->minutes = intval(config('app.elastic_store_time'));
+    }
+
+    public function handle()
+    {
+        $startDateTime = Carbon::now();
+        Log::info("Index Vehicles Process started at: " . $startDateTime);
+
+        $elasticsearch = app('Elasticsearch');
+        $url = config('app.cron_history_api_url') . '/cron-run-histories';
+
+        try {
+            // Remote Connection to KVM4.1
+            $cronRunResponse = Http::timeout(120)->retry(3, 1000)->get($url . '?name=process_vehicle_archiveds_to_elasticsearch');
+
+            if ($cronRunResponse->successful()) {
+                $lastCron = $cronRunResponse->json();
+                if ($lastCron && $lastCron['end_time']) {
+                    $endTime = Carbon::parse($lastCron['end_time']);
+                    $timeDifference = (int) max(0, $endTime->diffInMinutes(now()));
+
+                    if ($timeDifference > 20) {
+                        $this->minutes = $timeDifference + 10;
+                    } elseif ($timeDifference === 20) {
+                        $this->minutes = $timeDifference + 5;
+                    }
+                }
+            }
+
+            $cronRunResponse = Http::timeout(120)->retry(3, 1000)->post($url, [
+                'cron_name' => 'process_vehicle_archiveds_to_elasticsearch',
+                'start_time' => now(),
+                'status' => 'running',
+            ]);
+
+            if ($cronRunResponse->successful()) {
+                Log::info('STORE VEHICLE ARCHIVEDS TO ELASTICSEARCH CREATED');
+                $this->cronRun = $cronRunResponse->json()['id'] ?? null;
+            } else {
+                Log::info('Error: STORE VEHICLE ARCHIVEDS TO ELASTICSEARCH CREATED');
+            }
+
+        } catch (\Exception $e) {
+            Log::info("Error: STORE VEHICLE ARCHIVEDS TO ELASTICSEARCH: ", ['error' => $e->getMessage()]);
+            return;
+        }
+
+        $minutes = Carbon::now()->subMinutes($this->minutes);
+
+        if (config('app.is_full_fetch') == true) {
+            VehicleRecordArchived::chunk(500, function ($vehicles) use ($elasticsearch) {
+                foreach ($vehicles as $vehicle) {
+                    $elasticsearch->index([
+                        'index' => 'vehicle_record_archiveds',
+                        'id' => $vehicle->id,
+                        'body' => $vehicle->toArray(),
+                    ]);
+                }
+            });
+        } else {
+            VehicleRecordArchived::where('updated_at', '>=', $minutes)
+                ->chunk(500, function ($vehicles) use ($elasticsearch) {
+                    foreach ($vehicles as $vehicle) {
+                        $elasticsearch->index([
+                            'index' => 'vehicle_record_archiveds',
+                            'id' => $vehicle->id,
+                            'body' => $vehicle->toArray(),
+                        ]);
+                    }
+                });
+        }
+
+        if ($this->cronRun) {
+            $updateUrl = $url . "/$this->cronRun";
+            // Remote Connection to KVM4.1
+            $cronRunUpdateResponse = Http::timeout(120)->retry(3, 1000)->put($updateUrl, [
+                'end_time' => Carbon::now(),
+                'status' => 'success',
+                'updated_at' => now(),
+            ]);
+
+            if ($cronRunUpdateResponse->successful()) {
+                Log::info('STORE VEHICLE ARCHIVEDS TO ELASTICSEARCH CREATED');
+            } else {
+                Log::info('ERROR: STORE VEHICLE ARCHIVEDS TO ELASTICSEARCH CREATED');
+            }
+        }
+
+        Log::info('✅ Indexing vehicle_record_archiveds completed!');
+    }
+
+}
