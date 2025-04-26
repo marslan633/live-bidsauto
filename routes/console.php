be239b3046bfd6cf3ebb87c74849d989bbebd31c
= <=>
2025-04-26 20:32:54 +0500

    website: chore: Nextjs Setup done

--- routes/console.php
+++ routes/console.php
@@ -27,7 +27,7 @@ if (config('app.app_kvm_one') === true) {
     //     // Log the failure of process:api-data
     //     Log::error('process:api-data failed.');
     // });
-    app(Schedule::class)->command('process:api-data')->dailyAt('13:45')->withoutOverlapping();
+    app(Schedule::class)->command('process:api-data')->dailyAt('15:40')->withoutOverlapping();
     app(Schedule::class)->command('process:cached-data-without-queue')->everyFiveMinutes()->withoutOverlapping();
 
     // app(Schedule::class)->command('process:archived-data')->everyFifteenMinutes()->withoutOverlapping();
