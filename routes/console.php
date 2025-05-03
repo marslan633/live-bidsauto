2e339fdae0ef66963b7ccd65814d555b91df17a1
= <=>
2025-05-03 11:49:56 +0500

    test: Testing elasticsearch approach

--- routes/console.php
+++ routes/console.php
@@ -30,7 +30,7 @@ if (config('app.app_kvm_one') === true) {
     // app(Schedule::class)->command('process:api-data')->dailyAt('15:40')->withoutOverlapping();
     // app(Schedule::class)->command('process:cached-data-without-queue')->everyFiveMinutes()->withoutOverlapping();
 
-    // app(Schedule::class)->command('process:api-data')->dailyAt('15:40')->withoutOverlapping();
+    // app(Schedule::class)->command('process:api-data-with-elasticsearch')->dailyAt('15:40')->withoutOverlapping();
     // app(Schedule::class)->command('process:cached-data-with-elasticsearch')->everyFiveMinutes()->withoutOverlapping();
 
     // app(Schedule::class)->command('process:archived-data')->everyFifteenMinutes()->withoutOverlapping();
