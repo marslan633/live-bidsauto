4d8f3e66e4655c4bae9da926eb3382f981aa6e5c
= <=>
2025-05-05 14:45:48 +0500

    fix: Cron Run histories changed to elastiscsearch

--- routes/console.php
+++ routes/console.php
@@ -31,7 +31,7 @@ if (config('app.app_kvm_one') === true) {
     // app(Schedule::class)->command('process:cached-data-without-queue')->everyFiveMinutes()->withoutOverlapping();
     // app(Schedule::class)->command('process:archived-data')->everyFifteenMinutes()->withoutOverlapping();
 
-    app(Schedule::class)->command('process:api-data-with-elasticsearch')->dailyAt('09:45')->withoutOverlapping();
+    app(Schedule::class)->command('process:api-data-with-elasticsearch')->dailyAt('09:48')->withoutOverlapping();
     app(Schedule::class)->command('process:cached-data-with-elasticsearch')->everyFiveMinutes()->withoutOverlapping();
     // app(Schedule::class)->command('process:archived-data-with-elasticsearch')->everyFifteenMinutes()->withoutOverlapping();
 }
