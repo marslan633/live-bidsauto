0319d8a83e6d9938b9361d38ce59d4cae134ae4d
= <=>
2025-05-06 14:26:02 +0500

    fix: Delete commands added

--- routes/console.php
+++ routes/console.php
@@ -32,7 +32,7 @@ if (config('app.app_kvm_one') === true) {
     // app(Schedule::class)->command('process:cached-data-without-queue')->everyFiveMinutes()->withoutOverlapping();
     // app(Schedule::class)->command('process:archived-data')->everyFifteenMinutes()->withoutOverlapping();
 
-    app(Schedule::class)->command('process:api-data-with-elasticsearch')->dailyAt('09:50')->withoutOverlapping();
+    app(Schedule::class)->command('process:api-data-with-elasticsearch')->dailyAt('09:30')->withoutOverlapping();
     app(Schedule::class)->command('process:cached-data-with-elasticsearch')->everyFiveMinutes()->withoutOverlapping();
     // app(Schedule::class)->command('process:archived-data-with-elasticsearch')->everyFifteenMinutes()->withoutOverlapping();
 
