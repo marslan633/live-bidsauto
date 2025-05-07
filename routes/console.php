c245a70e30d8bd444dee024a0b85148ae32e38cf
= <=>
2025-05-07 14:38:03 +0500

    fix: Reset KVM One Command Added

--- routes/console.php
+++ routes/console.php
@@ -32,7 +32,7 @@ if (config('app.app_kvm_one') === true) {
     // app(Schedule::class)->command('process:cached-data-without-queue')->everyFiveMinutes()->withoutOverlapping();
     // app(Schedule::class)->command('process:archived-data')->everyFifteenMinutes()->withoutOverlapping();
 
-    app(Schedule::class)->command('process:api-data-with-elasticsearch')->dailyAt('10:05')->withoutOverlapping();
+    app(Schedule::class)->command('process:api-data-with-elasticsearch')->dailyAt('09:40')->withoutOverlapping();
     app(Schedule::class)->command('process:cached-data-with-elasticsearch')->everyFiveMinutes()->withoutOverlapping();
     // app(Schedule::class)->command('process:archived-data-with-elasticsearch')->everyFifteenMinutes()->withoutOverlapping();
 
