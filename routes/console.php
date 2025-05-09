5734a556c8b75eed494a7d237edd2dfd908f2869
= <=>
2025-05-09 11:16:30 +0500

    migratino updated

--- routes/console.php
+++ routes/console.php
@@ -85,7 +85,7 @@ if (config('app.app_kvm_three') === true) {
 
 if (config('app.app_kvm_four') === true) {
 
-    app(Schedule::class)->command('index:vehicle-records')->dailyAt('12:00')->withoutOverlapping();
+    app(Schedule::class)->command('index:vehicle-records')->dailyAt('06:20')->withoutOverlapping();
 
     // app(Schedule::class)->command('index:vehicle-record-archiveds')->dailyAt('11:10')->withoutOverlapping();
     // app(Schedule::class)->command('index:sale-auction-histories')->dailyAt('11:10')->withoutOverlapping();
