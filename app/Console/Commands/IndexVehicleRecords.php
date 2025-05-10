0ac89e0c1173042510d64ba43931dc1de1d6cdad
= <=>
2025-05-10 15:51:51 +0500

    migratino updated

--- app/Console/Commands/IndexVehicleRecords.php
+++ app/Console/Commands/IndexVehicleRecords.php
@@ -124,7 +124,7 @@ class IndexVehicleRecords extends Command
 
         // Set the chunk size to 10,000
         $chunkSize = 10000;
-        $dispatchChunkSize = 300;
+        $dispatchChunkSize = 500;
         // Set the starting point for the query
         $start = 0;
 
