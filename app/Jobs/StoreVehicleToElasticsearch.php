0ee5245f9cbc00c49c60e6f1feef358556ff069b
= <=>
2025-05-12 14:41:37 +0500

    migratino updated

--- app/Jobs/StoreVehicleToElasticsearch.php
+++ app/Jobs/StoreVehicleToElasticsearch.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Jobs;
 
 use Illuminate\Bus\Queueable;
@@ -9,12 +8,14 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Elasticsearch\ClientBuilder;
 use Illuminate\Support\Facades\Log;
+use App\Models\VehicleRecord;
 
 class StoreVehicleToElasticsearch implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     protected $vehicles;
+
     /**
      * Create a new job instance.
      *
@@ -37,13 +38,47 @@ class StoreVehicleToElasticsearch implements ShouldQueue
 
         $bulkData = [];
         foreach ($this->vehicles as $vehicle) {
+            // Eager load all relationships to avoid N+1 problem
+            $vehicle = $vehicle->load([
+                'manufacturer',
+                'vehicleModel',
+                'generation',
+                'bodyType',
+                'color',
+                'engine',
+                'transmission',
+                'driveWheel',
+                'vehicleType',
+                'fuel',
+                'status',
+                'seller',
+                'sellerType',
+                'titleRelation',
+                'detailedTitle',
+                'damageMain',
+                'damageSecond',
+                'condition',
+                'image',
+                'country',
+                'state',
+                'city',
+                'location',
+                'sellingBranch',
+                'buyNowRelation',
+            ]);
+
+            // Prepare bulk data for Elasticsearch
             $bulkData[] = ['index' => ['_index' => 'vehicle_records', '_id' => $vehicle->id]];
-            $bulkData[] = $vehicle->toArray();
+            $bulkData[] = $vehicle->toArray();  // Index the vehicle and all of its relations
+
+            // Optionally, log the vehicle data being indexed (for debugging)
+            Log::info('Indexing vehicle to Elasticsearch', ['vehicle_id' => $vehicle->id]);
         }
 
+        // Perform bulk indexing if there is data
         if (!empty($bulkData)) {
             $client->bulk(['body' => $bulkData]);
-            Log::info('Vehicle records indexed in Elasticsearch by job.');
+            Log::info('Vehicle records indexed in Elasticsearch.');
         }
     }
 }
