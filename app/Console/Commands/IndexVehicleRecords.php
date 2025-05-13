3de82f9786778c6692ae4e1971680b1bed395171
= <=>
2025-05-13 14:39:23 +0500

    migratino updated

--- app/Console/Commands/IndexVehicleRecords.php
+++ app/Console/Commands/IndexVehicleRecords.php
@@ -123,8 +123,8 @@ class IndexVehicleRecords extends Command
         $isFullFetch = config('app.is_full_fetch', false);
 
         // Set the chunk size to 10,000
-        $chunkSize = 10000;
-        $dispatchChunkSize = 300;
+        $chunkSize = 5000;
+        $dispatchChunkSize = 100;
         // Set the starting point for the query
         $start = 0;
         $query = VehicleRecord::query();
