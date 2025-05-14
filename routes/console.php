db5b3894139756ccd85a30dc513ec7a06a0ceecf
= <=>
2025-05-15 01:01:28 +0500

    fix: Delete Cached Archived Data With Elasticsearch

--- routes/console.php
+++ routes/console.php
@@ -90,7 +90,7 @@ if (config('app.app_kvm_four') === true) {
 //    app(Schedule::class)->command('index:vehicle-records')->dailyAt('16:57')->withoutOverlapping();
 
     app(Schedule::class)->command('index:vehicle-record-archiveds')->dailyAt('19:50')->withoutOverlapping();
-    app(Schedule::class)->command('index:sale-auction-histories')->dailyAt('19:50')->withoutOverlapping();
+    app(Schedule::class)->command('index:sale-auction-histories')->dailyAt('20:05')->withoutOverlapping();
 
     // app(Schedule::class)->command('process:delete-expired-data')->dailyAt('18:44')->withoutOverlapping();
 }
