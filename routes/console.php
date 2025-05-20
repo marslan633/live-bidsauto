2dbdb57607555727b4f2844e7c8a07eb4d01b25a
= <=>
2025-05-20 13:23:44 +0500

    testing archived recrods

--- routes/console.php
+++ routes/console.php
@@ -90,7 +90,7 @@ if (config('app.app_kvm_four') === true) {
 
 //    app(Schedule::class)->command('index:vehicle-records')->dailyAt('18:20')->withoutOverlapping();
 
-    app(Schedule::class)->command('index:vehicle-record-archiveds')->dailyAt('08:15')->withoutOverlapping();
+    app(Schedule::class)->command('index:vehicle-record-archiveds')->dailyAt('08:25')->withoutOverlapping();
     // app(Schedule::class)->command('index:sale-auction-histories')->dailyAt('20:25')->withoutOverlapping();
 
     // app(Schedule::class)->command('process:delete-expired-data')->dailyAt('18:44')->withoutOverlapping();
