0da6e87a1e8722e72d73c069ee92d9bde1d24b89
= <=>
2025-05-23 16:39:42 +0500

    fix

--- app/Console/Commands/ProcessApiDataWithElasticSearch.php
+++ app/Console/Commands/ProcessApiDataWithElasticSearch.php
@@ -44,7 +44,7 @@ class ProcessApiDataWithElasticSearch extends Command
 
         $hits = $response['hits']['hits'];
 
-        $minutes = 20;
+        $minutes = 4320;
 
         if (!empty($hits)) {
             $lastCron = $hits[0]['_source'];
