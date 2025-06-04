248852d4af2315629b19eea30fd6a989f69012b1
= <=>
2025-06-04 15:49:08 +0500

    Running Appi data command at 11:05

--- app/Console/Commands/KvmOne/ProcessApiDataWithElasticSearch.php
+++ app/Console/Commands/KvmOne/ProcessApiDataWithElasticSearch.php
@@ -44,7 +44,7 @@ class ProcessApiDataWithElasticSearch extends Command
 
         $hits = $response['hits']['hits'];
 
-        $minutes = 4320;
+        $minutes = 2000;
 
         if (!empty($hits)) {
             $lastCron = $hits[0]['_source'];
