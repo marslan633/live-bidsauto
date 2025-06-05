72a426eb66b66f98ef164d91a77c0d6f6b485865
= <=>
2025-06-05 20:08:08 +0500

    KVM4.1 Commands Settings Done

--- routes/console.php
+++ routes/console.php
@@ -2,10 +2,10 @@
 use Illuminate\Console\Scheduling\Schedule;
 
 if (config('app.app_kvm_one') === true) {
-     app(Schedule::class)->command('process:api-data-with-elasticsearch')->dailyAt('15:15')->withoutOverlapping();
+     app(Schedule::class)->command('process:api-data-with-elasticsearch')->dailyAt('15:30')->withoutOverlapping();
     //  app(Schedule::class)->command('process:api-data-with-elasticsearch')->cron('*/20 * * * *')->withoutOverlapping();
      app(Schedule::class)->command('process:cached-data-with-elasticsearch')->everyFiveMinutes()->withoutOverlapping();
-    //  app(Schedule::class)->command('process:archived-data-with-elasticsearch')->dailyAt('18:38')->withoutOverlapping();
+     app(Schedule::class)->command('process:archived-data-with-elasticsearch')->hourly()->withoutOverlapping();
     app(Schedule::class)->command('process:delete-cached-data-with-elasticsearch')->everyThirtyMinutes()->withoutOverlapping();
     // app(Schedule::class)->command('process:delete-cached-archived-data-with-elasticsearch')->hourly()->withoutOverlapping();
 }
