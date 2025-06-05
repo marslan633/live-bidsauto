6a05108e474a9390513910b9b7ab6070b240d3f2
= <=>
2025-06-05 16:24:25 +0500

    Resolving Issues on KVM4.3

--- app/Jobs/KvmThree/ProcessCachedArchivedDataJobWithElasticSearch.php
+++ app/Jobs/KvmThree/ProcessCachedArchivedDataJobWithElasticSearch.php
@@ -186,6 +186,7 @@ class ProcessCachedArchivedDataJobWithElasticSearch implements ShouldQueue
                             $newSaleRecord['seller_id'] = $existingRecords[$record['lot_id']]->seller_id;
                             $newSaleRecord['created_at'] = now();
                             $newSaleRecord['updated_at'] = now();
+                            Log::info('Sale Record 1', ['newSaleRecord' => json_encode($newSaleRecord)]);
                             $newSaleRecords[] = $newSaleRecord;
                         }
                     }elseif(!isset($existingSaleRecords[$record['lot_id']]) && isset($existingRecords[$record['lot_id']])){
@@ -197,6 +198,7 @@ class ProcessCachedArchivedDataJobWithElasticSearch implements ShouldQueue
                         $newSaleRecord['seller_id'] = $existingRecords[$record['lot_id']]->seller_id;
                         $newSaleRecord['created_at'] = now();
                         $newSaleRecord['updated_at'] = now();
+                        Log::info('Sale Record 2', ['newSaleRecord' => json_encode($newSaleRecord)]);
                         $newSaleRecords[] = $newSaleRecord;
                     }
 
