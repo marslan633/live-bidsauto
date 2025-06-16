143a1cf10c5acc60a19fafa5f8fa554fc1fa8099
= <=>
2025-06-16 17:12:37 +0500

    Search Qery Log Removed

--- app/Http/Controllers/Api/VehicleController.php
+++ app/Http/Controllers/Api/VehicleController.php
@@ -663,7 +663,7 @@ class VehicleController extends Controller
             $client = app('ElasticsearchKvmFour');
 
             // Debug Log: Check Index
-            Log::info('Search Index: ', ['index' => $index]);
+            // Log::info('Search Index: ', ['index' => $index]);
 
             $must = [['term' => ['data_source' => $dataSourceValue]]];
             // Base query for vehicle records
@@ -687,7 +687,7 @@ class VehicleController extends Controller
             }
 
             // Debug Log: Search Query
-            Log::info('Search Query: ', ['query' => $query]);
+            // Log::info('Search Query: ', ['query' => $query]);
 
             // Execute the main search query
             $response = $client->search($query);
