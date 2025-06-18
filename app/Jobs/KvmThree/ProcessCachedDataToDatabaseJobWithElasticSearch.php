7bf78ebe040adc652b67d576144a6ec6340b0b90
= <=>
2025-06-18 17:49:02 +0500

    Process Cached Data to Datasbe logic updated

--- app/Jobs/KvmThree/ProcessCachedDataToDatabaseJobWithElasticSearch.php
+++ app/Jobs/KvmThree/ProcessCachedDataToDatabaseJobWithElasticSearch.php
@@ -172,14 +172,14 @@ class ProcessCachedDataToDatabaseJobWithElasticSearch implements ShouldQueue
                         // Condition 3: Sale Date Not Macthed And Status == 3(sale) Active and Update
 
                         if($checkRecordSaleDate == $currentSaleDate && $checkRecordSaleDate->data_source == 2){
-                            Log::info('Contion 1', ['record' => json_encode($item)]);
+                            Log::info('Condition 1', ['record' => json_encode($item)]);
 
                             // DB::table('vehicle_records')->where('id', $getVehicleRecord->id)->update($item);
                         }elseif($checkRecordSaleDate != $currentSaleDate && $checkRecordSaleDate->status_id != 3 && $checkRecordSaleDate->data_source == 2){
-                            Log::info('Contion 2', ['record' => json_encode($item)]);
+                            Log::info('Condition 2', ['record' => json_encode($item)]);
                             // DB::table('vehicle_records')->where('id', $getVehicleRecord->id)->update($item);
                         }elseif($checkRecordSaleDate != $currentSaleDate && $checkRecordSaleDate->status_id == 3){
-                            Log::info('Contion 3', ['record' => json_encode($item)]);
+                            Log::info('Condition 3', ['record' => json_encode($item)]);
                             // $item['data_source'] = 1;
                             // DB::table('vehicle_records')->where('id', $getVehicleRecord->id)->update($item);
                         }
