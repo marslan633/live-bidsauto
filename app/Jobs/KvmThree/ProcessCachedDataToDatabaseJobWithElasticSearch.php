7481dc82efd8e350a5b891d7a0c2f54cf27e328c
= <=>
2025-06-18 18:20:43 +0500

    Process Cached Data to Datasbe logic updated

--- app/Jobs/KvmThree/ProcessCachedDataToDatabaseJobWithElasticSearch.php
+++ app/Jobs/KvmThree/ProcessCachedDataToDatabaseJobWithElasticSearch.php
@@ -172,22 +172,22 @@ class ProcessCachedDataToDatabaseJobWithElasticSearch implements ShouldQueue
                         // Condition 2: Sale Date Not Macthed And Status != 3 And data_source = 2 update or create auction history
                         // Condition 3: Sale Date Not Macthed And Status == 3(sale) Active and Update
 
-                        if($checkRecordSaleDate == $currentSaleDate && $checkRecordSaleDate->data_source == 2){
+                        if($checkRecordSaleDate == $currentSaleDate && $getVehicleRecord->data_source == 2){
                             Log::info('Condition 1', ['record' => json_encode($item)]);
 
                             // DB::table('vehicle_records')->where('id', $getVehicleRecord->id)->update($item);
-                        }elseif($checkRecordSaleDate != $currentSaleDate && $checkRecordSaleDate->status_id != 3 && $checkRecordSaleDate->data_source == 2){
+                        }elseif($checkRecordSaleDate != $currentSaleDate && $getVehicleRecord->status_id != 3 && $checkRecordSaleDate->data_source == 2){
                             Log::info('Condition 2', ['record' => json_encode($item)]);
                             // DB::table('vehicle_records')->where('id', $getVehicleRecord->id)->update($item);
-                        }elseif($checkRecordSaleDate != $currentSaleDate && $checkRecordSaleDate->status_id == 3){
+                        }elseif($checkRecordSaleDate != $currentSaleDate && $getVehicleRecord->status_id == 3){
                             Log::info('Condition 3', ['record' => json_encode($item)]);
                             // $item['data_source'] = 1;
                             // DB::table('vehicle_records')->where('id', $getVehicleRecord->id)->update($item);
                         }
 
 
-                        if(($checkRecordSaleDate == $currentSaleDate && $checkRecordSaleDate->data_source == 2)
-                        || ($checkRecordSaleDate != $currentSaleDate && $checkRecordSaleDate->status_id != 3 && $checkRecordSaleDate->data_source == 2)){
+                        if(($checkRecordSaleDate == $currentSaleDate && $getVehicleRecord->data_source == 2)
+                        || ($checkRecordSaleDate != $currentSaleDate && $getVehicleRecord->status_id != 3 && $getVehicleRecord->data_source == 2)){
                                 $saleAuctionRecord = DB::table('sale_auction_histories')
                                 ->where([
                                     'vin' => $item['vin'],
