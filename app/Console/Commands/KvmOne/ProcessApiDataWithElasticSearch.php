e34bb35823fc87d2fe71c19f89963b23f315cbbd
= <=>
2025-06-18 13:53:58 +0500

    lots_by_cartstat and lots_archived_by_carstats testing continue

--- app/Console/Commands/KvmOne/ProcessApiDataWithElasticSearch.php
+++ app/Console/Commands/KvmOne/ProcessApiDataWithElasticSearch.php
@@ -152,16 +152,16 @@ class ProcessApiDataWithElasticSearch extends Command
 
                             $lotsParams['body'][] = [
                                 'vin' => $item->vin,
-                                'lot_id' => $item->lots[0]->lot,
-                                'bid' => $item->lots[0]->bid,
-                                'bid_updated_at' => $item->lots[0]->bid_updated_at,
-                                'buy_now' => $item->lots[0]->buy_now,
-                                'buy_now_updated_at' => $item->lots[0]->buy_now_updated_at,
-                                'final_bid' => $item->lots[0]->final_bid,
-                                'final_bid_updated_at' => $item->lots[0]->final_bid_updated_at,
-                                'status' => $item->lots[0]->status->name,
-                                'carstat_created_at' => $item->lots[0]->created_at,
-                                'carstat_updated_at' => $item->lots[0]->updated_at,
+                                'lot_id' => $item->lots[0]['lot'],
+                                'bid' => $item->lots[0]['bid'],
+                                'bid_updated_at' => $item->lots[0]['bid_updated_at'],
+                                'buy_now' => $item->lots[0]['buy_now'],
+                                'buy_now_updated_at' => $item->lots[0]['buy_now_updated_at'],
+                                'final_bid' => $item->lots[0]['final_bid'],
+                                'final_bid_updated_at' => $item->lots[0]['final_bid_updated_at'],
+                                'status' => $item->lots[0]['status']['name'],
+                                'carstat_created_at' => $item->lots[0]['created_at'],
+                                'carstat_updated_at' => $item->lots[0]['updated_at'],
                                 'created_at' => $now->format('Y-m-d H:i:s'),
                                 'updated_at' => $now->format('Y-m-d H:i:s')
                             ];
