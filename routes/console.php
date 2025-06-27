1251cbde8fa2aa52035af8b3f3bcce24fac2038b
= <=>
2025-06-27 22:43:57 +0500

    Cleanring and Running again

--- routes/console.php
+++ routes/console.php
@@ -3,17 +3,17 @@
 use Illuminate\Console\Scheduling\Schedule;
 
 if (config('app.app_kvm_one') === true) {
-    //  app(Schedule::class)->command('process:api-data-with-elasticsearch')->dailyAt('22:44')->withoutOverlapping();
-        app(Schedule::class)->command('process:api-data-with-elasticsearch')->cron('*/30 * * * *')->withoutOverlapping();
+     app(Schedule::class)->command('process:api-data-with-elasticsearch')->dailyAt('22:44')->withoutOverlapping();
+        // app(Schedule::class)->command('process:api-data-with-elasticsearch')->cron('*/30 * * * *')->withoutOverlapping();
         app(Schedule::class)->command('process:cached-data-with-elasticsearch')->everyFiveMinutes()->withoutOverlapping();
-        app(Schedule::class)->command('process:archived-data-with-elasticsearch')->cron('52 * * * *')->withoutOverlapping();
-        app(Schedule::class)->command('process:delete-cached-data-with-elasticsearch')->cron('42 * * * *')->withoutOverlapping();
-        app(Schedule::class)->command('process:delete-cached-archived-data-with-elasticsearch')->cron('47 * * * *')->withoutOverlapping();
-        app(Schedule::class)->command('process:delete-api-data-with-elasticsearch')->cron('53 * * * *')->withoutOverlapping();
+        // app(Schedule::class)->command('process:archived-data-with-elasticsearch')->cron('52 * * * *')->withoutOverlapping();
+        // app(Schedule::class)->command('process:delete-cached-data-with-elasticsearch')->cron('42 * * * *')->withoutOverlapping();
+        // app(Schedule::class)->command('process:delete-cached-archived-data-with-elasticsearch')->cron('47 * * * *')->withoutOverlapping();
+        // app(Schedule::class)->command('process:delete-api-data-with-elasticsearch')->cron('53 * * * *')->withoutOverlapping();
 }
 
 if (config('app.app_kvm_three') === true) {
-    //app(Schedule::class)->command('process:process-cached-data-to-databases-with-elasticsearch')->everyTenMinutes()->withoutOverlapping();
+    app(Schedule::class)->command('process:process-cached-data-to-databases-with-elasticsearch')->everyTenMinutes()->withoutOverlapping();
 
     //app(Schedule::class)->command('process:cached-archived-data-to-database-with-elasticsearch')->cron('45 * * * *')->withoutOverlapping();
 
