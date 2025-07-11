5b87cdcbc049c7c711cc8e3835ccf9d54348c7fb
= <=>
2025-07-11 12:39:55 +0500

    Status Removed From ExpireAndStatusSale

--- app/Console/Commands/KvmThree/ExpireAndStatusSale.php
+++ app/Console/Commands/KvmThree/ExpireAndStatusSale.php
@@ -121,7 +121,7 @@ class ExpireAndStatusSale extends Command
 
             $totalArchived = 0;
             $query = VehicleRecord::query();
-            $query->where('data_source', 1)->where('status_id', 3)->whereRaw(
+            $query->where('data_source', 1)->whereRaw(
                 "DATE_FORMAT(STR_TO_DATE(sale_date, '%Y-%m-%dT%H:%i:%s.%fZ'), '%Y-%m-%d %H:%i') < ?",
                 [now()->format('Y-m-d H:i')]
             )->limit(5000);
--- routes/console.php
+++ routes/console.php
@@ -4,22 +4,22 @@ use Illuminate\Console\Scheduling\Schedule;
 
 if (config('app.app_kvm_one') === true) {
     //  app(Schedule::class)->command('process:api-data-with-elasticsearch')->dailyAt('18:15')->withoutOverlapping();
-        // app(Schedule::class)->command('process:api-data-with-elasticsearch')->cron('*/30 * * * *')->withoutOverlapping();
-     //   app(Schedule::class)->command('process:cached-data-with-elasticsearch')->everyFiveMinutes()->withoutOverlapping();
-        // app(Schedule::class)->command('process:archived-data-with-elasticsearch')->cron('52 * * * *')->withoutOverlapping();
-        // app(Schedule::class)->command('process:delete-cached-data-with-elasticsearch')->cron('42 * * * *')->withoutOverlapping();
-        // app(Schedule::class)->command('process:delete-cached-archived-data-with-elasticsearch')->cron('47 * * * *')->withoutOverlapping();
-        // app(Schedule::class)->command('process:delete-api-data-with-elasticsearch')->cron('53 * * * *')->withoutOverlapping();
+    // app(Schedule::class)->command('process:api-data-with-elasticsearch')->cron('*/30 * * * *')->withoutOverlapping();
+    //   app(Schedule::class)->command('process:cached-data-with-elasticsearch')->everyFiveMinutes()->withoutOverlapping();
+    // app(Schedule::class)->command('process:archived-data-with-elasticsearch')->cron('52 * * * *')->withoutOverlapping();
+    // app(Schedule::class)->command('process:delete-cached-data-with-elasticsearch')->cron('42 * * * *')->withoutOverlapping();
+    // app(Schedule::class)->command('process:delete-cached-archived-data-with-elasticsearch')->cron('47 * * * *')->withoutOverlapping();
+    // app(Schedule::class)->command('process:delete-api-data-with-elasticsearch')->cron('53 * * * *')->withoutOverlapping();
 }
 
 if (config('app.app_kvm_three') === true) {
-   // app(Schedule::class)->command('process:process-cached-data-to-databases-with-elasticsearch')->everyTenMinutes()->withoutOverlapping();
+    // app(Schedule::class)->command('process:process-cached-data-to-databases-with-elasticsearch')->everyTenMinutes()->withoutOverlapping();
 
-    //app(Schedule::class)->command('process:cached-archived-data-to-database-with-elasticsearch')->cron('45 * * * *')->withoutOverlapping();
+    // app(Schedule::class)->command('process:cached-archived-data-to-database-with-elasticsearch')->cron('45 * * * *')->withoutOverlapping();
 
-    //app(Schedule::class)->command('process:expire-and-status-sale-with-elasticsearch')->cron('52 * * * *')->withoutOverlapping();
+    // app(Schedule::class)->command('process:expire-and-status-sale-with-elasticsearch')->cron('52 * * * *')->withoutOverlapping();
 
-    //app(Schedule::class)->command('process:active-and-status-not-sale-with-elasticsearch')->cron('45 * * * *')->withoutOverlapping();
+    // app(Schedule::class)->command('process:active-and-status-not-sale-with-elasticsearch')->cron('45 * * * *')->withoutOverlapping();
 }
 
 if (config('app.app_kvm_four') === true) {
