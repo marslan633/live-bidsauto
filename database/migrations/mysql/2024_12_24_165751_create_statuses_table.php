23595f05535ee497955f3ece8a4e19aa041f83ac
HafizMUmerFarooq <hafizmumerfarooq10@gmail.com>
2025-11-01 14:18:31 +0500

    feat: allow statuses table to start with id=0 for default 'unknown' status before normal auto-increment to avoid status_id conflicts

--- database/migrations/mysql/2024_12_24_165751_create_statuses_table.php
+++ database/migrations/mysql/2024_12_24_165751_create_statuses_table.php
@@ -5,7 +5,6 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
-
 return new class extends Migration
 {
     /**
@@ -14,19 +13,26 @@ return new class extends Migration
     public function up(): void
     {
         Schema::connection('mysql')->create('statuses', function (Blueprint $table) {
-            $table->id();
+            $table->id(); // auto-incrementing primary key
             $table->unsignedBigInteger('status_api_id')->unique();
             $table->string('name')->nullable();
             $table->timestamps();
         });
 
-        
+        // Set auto-increment to start from 0
+        DB::statement('ALTER TABLE statuses AUTO_INCREMENT = 0;');
+
+        // Insert the "unknown" default record
         DB::connection('mysql')->table('statuses')->insertOrIgnore([
+            'id' => 0,
             'status_api_id' => 0,
             'name' => 'unknown',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
+
+        // Reset auto-increment to start from 1 after inserting the 0 record
+        DB::statement('ALTER TABLE statuses AUTO_INCREMENT = 1;');
     }
 
     /**
