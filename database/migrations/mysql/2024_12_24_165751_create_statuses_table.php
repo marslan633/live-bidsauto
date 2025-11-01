8de6f99c4854158dea61483f436452ee7b0ebe58
HafizMUmerFarooq <hafizmumerfarooq10@gmail.com>
2025-11-01 14:51:31 +0500

    feat: allow statuses table to start with id=0 v2

--- database/migrations/mysql/2024_12_24_165751_create_statuses_table.php
+++ database/migrations/mysql/2024_12_24_165751_create_statuses_table.php
@@ -7,23 +7,17 @@ use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::connection('mysql')->create('statuses', function (Blueprint $table) {
-            $table->id(); // auto-incrementing primary key
+            $table->unsignedBigInteger('id')->primary(); // no auto-increment
             $table->unsignedBigInteger('status_api_id')->unique();
             $table->string('name')->nullable();
             $table->timestamps();
         });
 
-        // Set auto-increment to start from 0
-        DB::statement('ALTER TABLE statuses AUTO_INCREMENT = 0;');
-
-        // Insert the "unknown" default record
-        DB::connection('mysql')->table('statuses')->insertOrIgnore([
+        // Insert the 'unknown' row with 0,0
+        DB::connection('mysql')->table('statuses')->insert([
             'id' => 0,
             'status_api_id' => 0,
             'name' => 'unknown',
@@ -31,13 +25,11 @@ return new class extends Migration
             'updated_at' => now(),
         ]);
 
-        // Reset auto-increment to start from 1 after inserting the 0 record
+        // Now convert id into auto_increment starting from 1
+        DB::statement('ALTER TABLE statuses MODIFY id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT;');
         DB::statement('ALTER TABLE statuses AUTO_INCREMENT = 1;');
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('statuses');
